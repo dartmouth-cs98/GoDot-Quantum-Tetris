36f33e74801c8f405c76424588980159eeb974a3
Phil-Hanlon <rafael.brantley@gmail.com>
2019-11-14 15:36:13 -0500

    http request implemented without endpoints

--- TopGUI.gd
+++ TopGUI.gd
@@ -8,6 +8,12 @@ var score = 0
 func _ready():
 	sc = get_node("HBoxContainer/VBoxContainer/HBoxContainer2/ScoreNum")
 	hsc = get_node("HBoxContainer/VBoxContainer/HBoxContainer2/HighScoreNum")
+	$HTTPRequest.request("endpoint_url", PoolStringArray(), false, HTTPClient.METHOD_GET, "username")
+	
+func _on_HTTPRequest_request_completed( result, response_code, headers, body ):
+	var response = JSON.parse(body.get_string_from_utf8())
+	sc = response.result.score
+	hsc = response.result.hiscore
 
 func _update_score(add):
 	score += add
